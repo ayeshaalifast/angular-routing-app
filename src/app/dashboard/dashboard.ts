705d0fc21258d3e2ef849e0b758a596c2b8e5033
AyeshaAlifast <i222128@nu.edu.pk>
2026-03-30 19:17:32 +0500

    Anglar Routing App - Commit Message

---
+++ src/app/dashboard/dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+}
